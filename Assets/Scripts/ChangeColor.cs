0b7926c964e6afb515b0e065ef5674803c1aa387
Juan De Leon <dele17822@uvg.edu.gt>
2019-04-22 11:08:48 -0600

    Firs commit

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Color startColor;
+    public Color hoverColor;
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", hoverColor);
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+
+    }
+
+    private void OnMouseDown()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        Physics.Raycast(myRay, out hitInfo);
+        rb.AddForce(-hitInfo.normal * 10, ForceMode.Impulse);
+        rb.useGravity = true;
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour
+{
+    private int ctr = 0;
+
+    public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Disparar();
+        }
+    }
+
+    void Disparar() {
+        
+        GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(myRay, out hitInfo, Mathf.Infinity) && hitInfo.collider.gameObject.tag == "Target")
+        {
+            ctr++;
+            Debug.Log("Contador de monedas: " + ctr);
+            score.text = "Score: " + ctr;
+            Destroy(hitInfo.collider.gameObject);
+        }
+    }
+}
